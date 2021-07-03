9d73cb1d63bec96867ad690f04be6ad5d7b760a6
Doctrina-lucem <sasha.m.dmitriev@gmail.com>
2021-07-03 17:21:24 +0300

    .

--- js/mainsSlider.js
+++ js/mainsSlider.js
@@ -1,7 +1,6 @@
 let series = document.querySelector(".pools-series__wrapper").children
 let title = document.querySelector(".series-description__title")
 console.log(series)
-let k = "kkkkkkk"
 for ( let elm of series) {
     elm.addEventListener("click", function () {
         title.textContent = elm.textContent
