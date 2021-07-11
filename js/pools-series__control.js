4ee02fe213c64a6f126eb9940d8f9a60d3cfd352
Doctrina-lucem <sasha.m.dmitriev@gmail.com>
2021-07-12 00:01:18 +0300

    .

---
+++ js/pools-series__control.js
@@ -0,0 +1,7 @@
+let control = document.querySelector(".pools-series__control")
+let list = document.querySelector(".pools-series__wrapper").children
+control.oninput = function (){
+    for (let y of list){
+
+    }
+}
