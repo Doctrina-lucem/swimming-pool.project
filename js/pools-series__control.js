386de8a0145f8edb9bf979af9c95384fbb9ca262
Doctrina-lucem <sasha.m.dmitriev@gmail.com>
2021-07-14 20:42:19 +0300

    .

--- js/pools-series__control.js
+++ js/pools-series__control.js
@@ -1,7 +1,7 @@
 let control = document.querySelector(".pools-series__control")
 let list = document.querySelector(".pools-series__wrapper").children
 control.oninput = function (){
-    for (let y of list){
-
-    }
+    console.log(list[control.value])
+    list[control.value-1].classList.remove("hide")
+    list[control.value-4].classList.add("hide")
 }
