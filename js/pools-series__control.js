adfa44e75ff17b4a86f238da70cc444190344da0
Doctrina-lucem <sasha.m.dmitriev@gmail.com>
2021-10-08 18:06:48 +0300

    .

--- js/pools-series__control.js
+++ js/pools-series__control.js
@@ -1,18 +1,29 @@
 let control = document.querySelector(".pools-series__control")
 let list = document.querySelector(".pools-series__wrapper").children
+if (document.documentElement.clientWidth < 425) {
+    control.value = 2
+}
 z = control.value
 control.oninput = function (){
-    if(~['Android', 'iPhone', 'iPod', 'iPad', 'BlackBerry'].indexOf(navigator.platform)) {
-
-    }
     console.log(control.value)
-    if (control.value < z){
-        list[control.value -3].classList.remove("hide")
-        list[control.value].classList.add("hide")
+    if (document.documentElement.clientWidth <= 425) {
+        if (control.value < z) {
+            list[control.value - 2].classList.remove("hide", "mobile-hide")
+            list[control.value].classList.add("hide")
+        } else {
+            list[control.value - 1].classList.remove("hide", "mobile-hide")
+            list[control.value - 3].classList.add("hide")
+        }
+        z = control.value
     }
     else {
-        list[control.value - 1].classList.remove("hide")
-        list[control.value - 4].classList.add("hide")
+        if (control.value < z) {
+            list[control.value - 3].classList.remove("hide")
+            list[control.value].classList.add("hide")
+        } else {
+            list[control.value - 1].classList.remove("hide")
+            list[control.value - 4].classList.add("hide")
+        }
+        z = control.value
     }
-    z = control.value
 }
